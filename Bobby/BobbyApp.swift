a8e0ec442ad11534fd4bdc5fd8f1db7292ae25b6
Nitin <frillyboylikeyou@gmail.com>
2025-08-02 09:53:54 +0800

    Initial Commit

---
+++ Bobby/BobbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BobbyApp.swift
+//  Bobby
+//
+//  Created by Administrator on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BobbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BobbyTests/BobbyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BobbyTests.swift
+//  BobbyTests
+//
+//  Created by Administrator on 2/8/25.
+//
+
+import Testing
+@testable import Bobby
+
+struct BobbyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
