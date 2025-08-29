a7eace31a0470e83c6746f4afcbff038bfe21711
Nitin <frillyboylikeyou@gmail.com>
2025-08-29 18:34:06 +0800

    latest version

--- Dance Detector/Dance Detector/ContentView.swift
+++ Dance Detector/Dance Detector/ContentView.swift
@@ -63,8 +63,37 @@ struct ContentView: View {
                     y: 0.0
                 )
                 .buttonStyle(.borderedProminent)
+        Text("Overall Points: (will be determined with another variable)")
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.yellow)
+            .cornerRadius(10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.orange, lineWidth: 2)
+            )
+            .padding(.horizontal)
+        
+        Text("Song Lyrics: (will add something here later)")
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(Color.yellow)
+            .cornerRadius(10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.green, lineWidth: 2)
+            )
+            .padding(.horizontal)
+            
+        Spacer()
+        Rectangle()
+                    .fill(Color.blue)
+                    .frame(width: 1, height: 1000)
+        Spacer()
             }
 
         }
-    
 
+#Preview {
+    ContentView()
+}
